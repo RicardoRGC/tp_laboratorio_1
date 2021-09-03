bef8d45367158c1fa7a7485e42eb15f0ba0d2be7
RicardoRGC <gonzalezricardo.c90@gmail.com>
2021-09-03 10:43:39 -0300

    tp numero 1

---
+++ TP_[1]/TP_[1].c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP_[1].c
+ Author      : Gonzalez Ricardo Celso
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
